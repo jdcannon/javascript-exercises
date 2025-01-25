5b12e1ef0dccdaf3b6c909657e847f7767b4cd58
James Cannon <promethorn@outlook.com>
2025-01-25 16:10:08 -0500

    Ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+  const result = arr.filter((value) => !values.includes(value));
+  return result;
+
 };
 
 // Do not edit below this line
