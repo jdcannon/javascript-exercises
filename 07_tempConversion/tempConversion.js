e73ba19ebe733721b249b3601270a171698481c7
James Cannon <promethorn@outlook.com>
2025-01-25 17:00:44 -0500

    Ex7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const tempC = (tempF - 32) * (5 / 9);
+  return Number.parseFloat(tempC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  const tempF = (tempC * ( 9 / 5 )) + 32;
+  return Number.parseFloat(tempF.toFixed(1));
 };
 
 // Do not edit below this line
