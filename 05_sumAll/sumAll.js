4627d6818d8e09524d8927a5298c35fb783685df
James Cannon <promethorn@outlook.com>
2025-01-25 16:28:54 -0500

    Ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if ( !((typeof(a) == "number") && (a > 0) && (a%1 == 0)) ) {
+    return "ERROR";
+  }
+  if ( !((typeof(b) == "number") && (b > 0) && (b%1 == 0)) ) {
+    return "ERROR";
+  }
+
+  let numFrom = Math.min(a, b)
+  let numTo = Math.max(a, b)
+
+  let acc = 0
+  for (let i = numFrom; i <= numTo; i++) {
+    acc += i;
+  }
+  return acc;
 
 };
 
