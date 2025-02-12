d1985e990750336e915b8851c314a9c39e542c00
James Cannon <promethorn@outlook.com>
2025-02-12 11:35:11 -0500

    Ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const checkAge = function(person){
+    if (Object.hasOwn(person, "yearOfDeath")) {
+      return person.yearOfDeath - person.yearOfBirth;
+    } else {
+      return 2025 - person.yearOfBirth;
+    }
+  }
+
+  let oldest = [0, {}];
+  people.forEach( (person) => {
+    let age = checkAge(person);
+    if (oldest[0] < age) {
+      oldest[0] = age;
+      oldest[1] = person;
+    }
+  })
+  return oldest[1];
 
 };
 
