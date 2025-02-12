8bff57a910951d608ea5291dbc2f3eafab576901
James Cannon <promethorn@outlook.com>
2025-02-12 10:54:40 -0500

    Ex9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+  let allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789".split('');
+  let testedPhrase = phrase.toLowerCase().split('').filter( (letter) => {
+    return allowedChars.includes(letter);
+  });
+  let reversed = testedPhrase.toReversed();
+  return reversed.reduce( (acc, curr, idx) => {
+    return acc && testedPhrase[idx] === curr;
+  }, true);
+  
 };
 
 // Do not edit below this line
