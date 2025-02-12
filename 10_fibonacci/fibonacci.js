1892905708744a73349fbbcb825d37af9406c58d
James Cannon <promethorn@outlook.com>
2025-02-12 11:12:28 -0500

    Ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  if (typeof(number) == "string") {
+    number = Number.parseInt(number);
+  }
+  if (number < 0) {
+    return "OOPS";
+  } else if (number == 0) {
+    return 0;
+  }
+  let valuePrev = 0;
+  let valueCurr = 1;
+  for (let i = 1; i < number; i++) {
+    let temp = valueCurr + valuePrev;
+    valuePrev = valueCurr;
+    valueCurr = temp;
+  }
+  return valueCurr;
 };
 
 // Do not edit below this line
